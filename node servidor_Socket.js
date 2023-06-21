09d9e116ba7fc23af1884d3ca020fee45e715e2f
ZeckDev <122693027+Zeck47@users.noreply.github.com>
2023-06-21 08:28:45 -0300

    Update node servidor_Socket.js

--- node servidor_Socket.js
+++ node servidor_Socket.js
@@ -1,20 +1,20 @@
-const net = require('net');
-const port = 8000;
-const host = '127.0.0.1';
+const net = require('net'); // A requisição da Net
+const port = 8000; // Configurar porta de entrada do host
+const host = '127.0.0.1'; // Tipo de hospedagem do servidor
 
 const server = net.createServer((socket) => {
-  console.log('Cliente conectado');
+  console.log('Cliente conectado'); // Mensagem enviada ápos o clinte conectar ao servidor
 
   socket.on('data', (data) => {
     console.log('Dados recebidos:', data.toString());
-    socket.write(data);
+    socket.write(data); // Será recebida os dados da coexão do cliente hospedado ao servidor
   });
 
   socket.on('end', () => {
     console.log('Cliente desconectado');
-  });
+  }); // receberá esta mensagem ápos o cliente do servidor desconectar
 });
 
 server.listen(port, host, () => {
   console.log(Servidor ouvindo em ${host}:${port});
-});
+}); // Mensagem enviada com o tipo de conexão que o socket está sendo executado
